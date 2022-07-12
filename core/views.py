9708ea3ebac0f218238593f133f76c83c90f504a
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-07-12 12:18:31 +0430

    Authentication for form

--- core/views.py
+++ core/views.py
@@ -1,15 +1,15 @@
-import pathlib
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from core.models import Mail
-from django.http import HttpResponse, FileResponse
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import CreateView
 from django.views import View
-from django.conf import settings
+from django.views.generic import CreateView
+
+from core.models import Mail
 
 
-class MailFormView (CreateView):
+class MailFormView (LoginRequiredMixin, CreateView):
+    login_url = '/admin/'
     model = Mail
     fields = ['to', 'subject', 'message']
     success_url = reverse_lazy('form')
