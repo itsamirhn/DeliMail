628e32bec7407cca9217b00b9a02f0830c9261b7
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-07-12 12:12:14 +0430

    Update Database

--- DeliMail/settings.py
+++ DeliMail/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
 import environ
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
@@ -84,11 +85,15 @@ WSGI_APPLICATION = 'DeliMail.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///' + (BASE_DIR / 'db.sqlite3').__str__())
 }
 
 
